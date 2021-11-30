1b9981aa95db8c72d3ce54e3041b34a3fe31cc1a
LTabak <LTabak@CSLL25.cornellcollege.edu>
2021-11-30 10:00:15 -0600

    Add Constructor and Tostring method

--- src/length/Length.java
+++ src/length/Length.java
@@ -2,9 +2,25 @@
 package length;
 
 public class Length {
-
+    public static final int INCHES_IN_A_FOOT=12;
+    
+    private int feet;
+    private int inches;
+    
+    public Length( int feet, int inches){
+        this.feet = feet;
+        this.inches = inches;
+    }
+    
+    @Override
+    public String toString(){
+        return this.feet + " feet, " + this.inches + " inches";
+    }
+    
     public static void main(String[] args) {
         System.out.println("Hello");
+        Length a=new Length(5, 11);
+        System.out.println(a);
     }
     
 }
